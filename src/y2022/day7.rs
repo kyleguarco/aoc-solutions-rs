37cdd87e041adbd3603a95070edbcbbf4adc3722
Kyle Guarco <kyleguarco55@gmail.com>
2023-01-02 02:11:28 -0500

    Day 7 Mid

--- src/y2022/day7.rs
+++ src/y2022/day7.rs
@@ -1,143 +1,316 @@
-use std::{collections::HashMap, ops::Deref};
+use std::{collections::HashMap, ptr::null_mut};
 
 use crate::get_input;
 
-#[derive(PartialEq)]
-enum NodeType {
-	File,
-	Directory,
+#[derive(Debug)]
+enum Command<'a> {
+	List,
+	Change(&'a str),
 }
 
-struct Metadata {
+impl<'a> TryFrom<&'a str> for Command<'a> {
+	type Error = ();
+
+	fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+		if !s.starts_with("$") {
+			return Err(());
+		}
+
+		let mut s = s.split_whitespace().skip(1);
+
+		match s.next().ok_or(())? {
+			"ls" => Ok(Self::List),
+			"cd" => Ok(Self::Change(s.next().ok_or(())?)),
+			_ => Err(()),
+		}
+	}
+}
+
+#[derive(Debug)]
+enum Input<'a> {
+	Command(Command<'a>),
+	Directory(&'a str),
+	File(usize, &'a str),
+}
+
+impl<'a> TryFrom<&'a str> for Input<'a> {
+	type Error = ();
+
+	fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+		let mut words = s.split_whitespace();
+		let start = words.next().ok_or(())?;
+
+		match start {
+			"$" => Ok(Self::Command(s.try_into()?)),
+			"dir" => Ok(Self::Directory(words.next().ok_or(())?)),
+			// Handle the case where the first word is a file size
+			_ => {
+				let size = start.parse::<usize>().map_err(|_| ())?;
+				let name = words.next().ok_or(())?;
+				Ok(Self::File(size, name))
+			}
+		}
+	}
+}
+
+#[derive(Clone, Debug)]
+enum PathError {
+	AtRoot,
+	NameContainsSlash,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct Path {
+	pstr: String,
+}
+
+impl Path {
+	fn new() -> Self {
+		Self { pstr: String::from("/") }
+	}
+
+	fn abs_path(&self) -> &str {
+		&self.pstr
+	}
+
+	fn name(&self) -> &str {
+		let mut iter = self.pstr.split('/');
+		iter.next_back();
+		// SAFETY: By this point, the files have names, except for root.
+		iter.next_back().unwrap()
+	}
+
+	fn parent(&mut self) -> Result<&str, PathError> {
+		if self.abs_path() == "/" {
+			return Err(PathError::AtRoot);
+		}
+
+		self.pstr.pop(); // pop off ending "/"
+		loop {
+			if self.pstr.ends_with("/") {
+				break;
+			} else {
+				self.pstr.pop();
+			}
+		}
+
+		Ok(&self.pstr)
+	}
+
+	fn child(&mut self, name: &str) -> Result<&str, PathError> {
+		if name.contains("/") {
+			return Err(PathError::NameContainsSlash);
+		}
+
+		self.pstr.push_str(name);
+		self.pstr.push('/');
+
+		Ok(&self.pstr)
+	}
+}
+
+type NodeID = usize;
+
+#[derive(Clone, Debug)]
+struct File {
+	inode: NodeID,
 	size: usize,
-	ntype: NodeType,
+	path: Path,
 }
 
-impl Metadata {
-	fn new(ntype: NodeType, size: usize) -> Self {
-		Self { size, ntype }
-	}
-
-	fn is_file(&self) -> bool {
-		self.ntype == NodeType::File
-	}
-
-	fn is_dir(&self) -> bool {
-		self.ntype == NodeType::Directory
+impl File {
+	fn new(inode: NodeID, size: usize, path: Path) -> Self {
+		Self { inode, size, path }
 	}
 }
 
-struct Node {
-	parent: Option<*mut Self>,
-	meta: Metadata,
-	children: Option<HashMap<String, Self>>,
+#[derive(Clone, Debug)]
+struct Directory {
+	inode: NodeID,
+	size: usize,
+	path: Path,
+	children: Vec<NodeID>,
+}
+
+impl Directory {
+	fn new(inode: NodeID, size: usize, path: Path) -> Self {
+		let children = Vec::new();
+		Self { inode, size, path, children }
+	}
+
+	fn add_as_child(&mut self, inode: NodeID) {
+		self.children.push(inode);
+		self.children.sort_unstable();
+	}
+
+	fn is_child(&self, inode: NodeID) -> bool {
+		self.children.binary_search(&inode).is_ok()
+	}
+
+	fn children(&self) -> &Vec<NodeID> {
+		&self.children
+	}
+}
+
+#[derive(Clone, Debug)]
+enum Node {
+	File(File),
+	Directory(Directory),
 }
 
 impl Node {
-	fn new(ntype: NodeType, size: usize, parent: Option<*mut Self>) -> Self {
-		let children = match ntype {
-			NodeType::File => None,
-			NodeType::Directory => Some(HashMap::new()),
-		};
-		Self {
-			parent,
-			meta: Metadata::new(ntype, size),
-			children,
+	fn inode(&self) -> NodeID {
+		match self {
+			Node::File(file) => file.inode,
+			Node::Directory(dir) => dir.inode,
 		}
 	}
 
-	fn is_orphan(&self) -> bool {
-		self.parent.is_none()
-	}
-
-	fn children(&self) -> &HashMap<String, Self> {
-		self.children.as_ref().unwrap()
-	}
-
-	fn children_mut(&mut self) -> &mut HashMap<String, Self> {
-		self.children.as_mut().unwrap()
-	}
-
 	fn size(&self) -> usize {
-		self.meta.size
-		// For being iterative:
-		// match self.meta.ntype {
-		// 	NodeType::File => self.meta.size,
-		// 	NodeType::Directory => self.children
-		// 		.into_iter()
-		// 		.map(|node| {
-		// 			node
-		// 			.values()
-		// 			.map(|child| child.size())
-		// 			.sum::<usize>()
-		// 		})
-		// 		.sum(),
-		// }
+		match self {
+			Node::File(file) => file.size,
+			Node::Directory(dir) => dir.size,
+		}
+	}
+
+	fn abs_path(&self) -> &str {
+		match self {
+			Node::File(file) => file.path.abs_path(),
+			Node::Directory(dir) => dir.path.abs_path(),
+		}
+	}
+
+	fn name(&self) -> &str {
+		match self {
+			Node::File(file) => file.path.name(),
+			Node::Directory(dir) => dir.path.name(),
+		}
+	}
+
+	fn as_dir_mut(&mut self) -> Option<&mut Directory> {
+		match self {
+			Node::File(_) => None,
+			Node::Directory(dir) => Some(dir),
+		}
 	}
 }
 
+#[derive(Clone, Debug)]
 struct FileSystem {
-	root: Node,
-	cwd: *mut Node,
+	index: NodeID,
+	cwd: *mut Directory,
+	names: HashMap<String, NodeID>,
+	nodes: HashMap<NodeID, Node>,
 }
 
 impl FileSystem {
 	fn new() -> Self {
-		let mut root = Node::new(NodeType::Directory, 0, None);
-		// SAFETY: We'll always have a root for as long as the filesystem is alive.
-		let cwd: *mut Node = &mut root as *mut Node;
-		Self { root, cwd }
+		let mut fs = Self {
+			index: 0,
+			cwd: null_mut(),
+			names: HashMap::new(),
+			nodes: HashMap::new(),
+		};
+		let root = Node::Directory(Directory::new(0, 0, Path::new()));
+
+		fs.names.insert(root.abs_path().to_owned(), 0);
+		fs.nodes.insert(0, root);
+
+		// SAFETY: We just created this object
+		let root = fs.nodes.get_mut(&0).unwrap();
+		fs.cwd = root.as_dir_mut().unwrap() as *mut Directory;
+
+		fs
 	}
 
-	fn get_cwd(&mut self) -> &mut Node {
-		// SAFETY: We'll always have a root for as long as the filesystem is alive.
-		unsafe { self.cwd.as_mut().unwrap() }
+	fn cwd_as_ref(&self) -> &Directory {
+		// SAFETY: `self.cwd` was set in `new` and `cd`, it will never be null.
+		unsafe { self.cwd.as_ref().expect("Oops, cwd was null :(") }
 	}
 
-	fn create_file(&mut self, path: &str, size: usize) -> bool {
-		let cwd = self.get_cwd();
-
-		let node = Node::new(NodeType::File, size, Some(cwd as *mut Node));
-		cwd.meta.size += size;
-
-		cwd.children_mut().insert(path.to_string(), node).is_some()
+	fn cwd_as_mut(&mut self) -> &mut Directory {
+		// SAFETY: `self.cwd` was set in `new` and `cd`, it will never be null.
+		unsafe { self.cwd.as_mut().expect("Oops, cwd was null :(") }
 	}
 
-	fn create_dir(&mut self, path: &str) -> bool {
-		let cwd = self.get_cwd();
-
-		let node = Node::new(NodeType::Directory, 0, Some(cwd as *mut Node));
-
-		// SAFETY: We guarantee `cwd` is a directory in `self.cd`
-		cwd.children_mut().insert(path.to_string(), node).is_some()
-	}
-
-	fn cd(&mut self, path: &str) -> bool {
-		let parent = unsafe { self.cwd.as_ref().unwrap().parent.as_ref() };
-		if parent.is_some() && path == ".." {
-			self.cwd = *parent.unwrap();
-			return true;
-		}
-
-		let cwd = self.get_cwd();
-
-		match cwd.children_mut().get_mut(path) {
-			Some(nwd) => match nwd.meta.ntype {
-				NodeType::File => false,
-				NodeType::Directory => {
-					self.cwd = nwd as *mut Node;
-					true
+	fn cd(&mut self, name: &str) {
+		if name == ".." {
+			let mut path = self.cwd_as_ref().path.clone();
+			path.parent().expect("At root?");
+			let nid = self.names.get(path.abs_path()).expect("Path not parsed right?");
+			let nwd =
+				self.nodes.get_mut(nid).expect("Bad INode cd?").as_dir_mut().expect("isn't dir");
+			self.cwd = nwd as *mut Directory;
+		} else {
+			if name.starts_with("/") {
+				// Handle a case where the name is an abs_path
+				let nid = self.names.get(name).expect("Bad name");
+				let nwd = self.nodes.get_mut(nid).expect("Bad INode").as_dir_mut().expect("isn't dir");
+				self.cwd = nwd as *mut Directory;
+			} else {
+				for inode in self.cwd_as_ref().children().iter().cloned() {
+					let nwd = self.nodes.get(&inode).expect("Bad INode");
+					if nwd.name() == name {
+						let nwd = self.nodes.get_mut(&inode).unwrap().as_dir_mut().expect("isn't dir");
+						self.cwd = nwd as *mut Directory;
+						return;
+					}
 				}
-			},
-			None => false,
+			}
 		}
+	}
+
+	fn allocate(&mut self, node: Node) {
+		self.index += 1;
+		let index = self.index;
+
+		self.cwd_as_mut().add_as_child(index);
+
+		self.names.insert(node.abs_path().to_owned(), index);
+		self.nodes.insert(index, node);
+	}
+
+	fn new_file(&mut self, name: &str, size: usize) {
+		self.cwd_as_mut().size += size;
+
+		let mut path = self.cwd_as_ref().path.clone();
+		path.child(name).expect("The name had a slash in it :(");
+
+		self.allocate(Node::File(File::new(self.index, size, path)))
+	}
+
+	fn new_dir(&mut self, name: &str) {
+		let mut path = self.cwd_as_ref().path.clone();
+		path.child(name).expect("The name had a slash in it :(");
+
+		self.allocate(Node::Directory(Directory::new(self.index, 0, path)))
+	}
+}
+
+impl<'a, I: Iterator<Item = Input<'a>>> From<I> for FileSystem {
+	fn from(inputs: I) -> Self {
+		let mut fs = Self::new();
+
+		for ip in inputs {
+			match ip {
+				Input::Command(cmd) => match cmd {
+					Command::List => continue,
+					Command::Change(name) => fs.cd(name),
+				},
+				Input::Directory(name) => fs.new_dir(name),
+				Input::File(size, name) => fs.new_file(name, size),
+			}
+		}
+
+		fs
 	}
 }
 
 #[test]
 fn test_part1() {
 	let input = get_input("day_7_test.txt");
-	let mut fs = FileSystem::new();
+
+	let fs = FileSystem::from(input.lines().map(|s| Input::try_from(s).expect("Bad Input")));
 }
 
 #[test]
