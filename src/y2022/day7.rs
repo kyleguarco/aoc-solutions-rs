734987fbb70cf2f61686fbc947efb88eef5f59c9
Kyle Guarco <kyleguarco55@gmail.com>
2023-01-14 18:45:51 -0500

    2022 Day 7
    THIS TOOK FOREVER! I wrote a pretend filesystem SAFELY.

--- src/y2022/day7.rs
+++ src/y2022/day7.rs
@@ -2,6 +2,9 @@ use std::{collections::HashMap, fmt::Display};
 
 use crate::get_input;
 
+type NodeID = usize;
+type Filesize = usize;
+
 #[derive(Debug)]
 enum Command<'a> {
 	List,
@@ -30,7 +33,7 @@ impl<'a> TryFrom<&'a str> for Command<'a> {
 enum Input<'a> {
 	Command(Command<'a>),
 	NewDirectory(&'a str),
-	NewFile(usize, &'a str),
+	NewFile(Filesize, &'a str),
 }
 
 impl<'a> TryFrom<&'a str> for Input<'a> {
@@ -99,19 +102,17 @@ impl Display for Path {
 	}
 }
 
-type NodeID = usize;
-
 #[derive(Clone, Debug)]
 struct ChildDirectory {
 	inode: NodeID,
 	parent: NodeID,
-	size: usize,
+	size: Filesize,
 	path: Path,
 	children: Vec<NodeID>,
 }
 
 impl ChildDirectory {
-	fn new(inode: NodeID, parent: NodeID, size: usize, path: Path) -> Self {
+	fn new(inode: NodeID, parent: NodeID, size: Filesize, path: Path) -> Self {
 		Self {
 			inode,
 			parent,
@@ -128,7 +129,7 @@ impl ChildDirectory {
 
 #[derive(Clone, Debug)]
 struct RootDirectory {
-	size: usize,
+	size: Filesize,
 	path: Path,
 	children: Vec<NodeID>,
 }
@@ -154,7 +155,7 @@ impl RootDirectory {
 #[derive(Clone, Debug)]
 enum Node {
 	// It's constructed like this to save lines; We never reference File.
-	File((NodeID, usize, Path)),
+	File((NodeID, Filesize, Path)),
 	Directory(ChildDirectory),
 	Root(RootDirectory),
 }
@@ -172,7 +173,7 @@ impl Node {
 		Ok(())
 	}
 
-	fn upsize(&mut self, upsize: usize) {
+	fn upsize(&mut self, upsize: Filesize) {
 		let size: &mut usize = match self {
 			Self::File(file) => &mut file.1,
 			Self::Directory(dir) => &mut dir.size,
@@ -198,7 +199,7 @@ impl Node {
 		}
 	}
 
-	fn size(&self) -> usize {
+	fn size(&self) -> Filesize {
 		match self {
 			Self::File(file) => file.1,
 			Self::Directory(dir) => dir.size,
@@ -218,6 +219,13 @@ impl Node {
 			Self::File(_) => None,
 			Self::Directory(dir) => Some(dir),
 			Self::Root(_) => None,
+		}
+	}
+
+	fn is_root(&self) -> bool {
+		match self {
+			Self::Root(_) => true,
+			_ => false,
 		}
 	}
 }
@@ -248,20 +256,24 @@ impl FileSystem {
 		}
 	}
 
+	/// Obtains the current working directory immutably.
 	fn cwd_as_ref(&self) -> &Node {
 		// SAFETY: We know this node exists. We ensure its existence in self.cd
 		self.nodes.get(&self.cwd).unwrap()
 	}
 
+	/// Obtains the current working directory mutably.
 	fn cwd_as_mut(&mut self) -> &mut Node {
 		// SAFETY: We know this node exists. We ensure its existence in self.cd
 		self.nodes.get_mut(&self.cwd).unwrap()
 	}
 
+	/// Yields an iterator of all the (immutable) nodes allocated to the filesystem.
 	fn nodes(&self) -> impl Iterator<Item = &Node> {
 		self.nodes.values()
 	}
 
+	/// Clones the path object of `self.cwd` and appends to it.
 	fn derive_path(&self, name: &str) -> Result<Path, IOError> {
 		self.cwd_as_ref().path().clone().child(name)
 	}
@@ -279,7 +291,7 @@ impl FileSystem {
 		Ok(())
 	}
 
-	fn new_file(&mut self, name: &str, size: usize) -> Result<(), IOError> {
+	fn new_file(&mut self, name: &str, size: Filesize) -> Result<(), IOError> {
 		let path = self.derive_path(name)?;
 
 		self.allocate(Node::File((self.next_index, size, path)))?;
@@ -360,32 +372,75 @@ impl<'a, I: Iterator<Item = Input<'a>>> TryFrom<FSInput<I>> for FileSystem {
 	}
 }
 
+// The size constant for part 1
+const DELETABLE_SIZE: Filesize = 100_000;
+
+// ...and the ones for part 2.
+const TOTAL_SPACE: Filesize = 70_000_000;
+const REQUIRED_SPACE: Filesize = 30_000_000;
+
+fn create_fs(input: &str) -> (FileSystem, Filesize) {
+	let mut fs = FileSystem::try_from(FSInput(
+		input
+			.lines()
+			.map(|s| Input::try_from(s).expect("Bad Input.")),
+	))
+	.expect("Bad Conversion.");
+
+	fs.cd("/").expect("Couldn't go to top.");
+	let used_space = fs.cwd_as_ref().size();
+	let unused_space = TOTAL_SPACE
+		.checked_sub(used_space)
+		.expect("The size don't fit boi.");
+	let required_space = REQUIRED_SPACE.saturating_sub(unused_space);
+	println!(
+		"Filesystem Size: {} ({} unused, {} required for update)",
+		used_space, unused_space, required_space
+	);
+
+	(fs, required_space)
+}
+
 #[test]
 fn part1() {
 	let input = get_input("day_7.txt");
 
-	let mut fs = FileSystem::try_from(FSInput(
-		input
-			.lines()
-			.map(|s| Input::try_from(s).expect("Bad Input")),
-	))
-	.unwrap();
-
-	fs.cd("/").unwrap();
-	println!("Root Size: {}", fs.cwd_as_ref().size());
+	let (fs, _) = create_fs(&input);
 
 	let sum = fs
 		.nodes()
 		.filter_map(|n| {
-			if n.is_dir() && n.size() <= 100000 {
+			if n.is_dir() && n.size() <= DELETABLE_SIZE {
 				Some(n.size())
 			} else {
 				None
 			}
 		})
 		.sum::<usize>();
-	println!("Directory summation: {sum}");
+	println!("Total size of small directories: {sum}");
 }
 
 #[test]
-fn part2() {}
+fn part2() {
+	let input = get_input("day_7.txt");
+
+	let (fs, required_space) = create_fs(&input);
+
+	let mut size = fs
+		.nodes()
+		.filter_map(|n| {
+			// Filter all of the directories (excluding root)
+			if n.is_dir() && !n.is_root() {
+				Some(n.size())
+			} else {
+				None
+			}
+		})
+		// Collect it into a Vec so it can be sorted.
+		.collect::<Vec<_>>();
+	size.sort_unstable();
+	// Find the first size that is larger than the required space.
+	let size = size.into_iter().find(|s| *s >= required_space).unwrap();
+
+	println!("Size of directory to be deleted: {size}");
+}
